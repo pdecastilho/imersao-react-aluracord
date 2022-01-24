8e74fad3c0d9e04fcce1104fe9b0d952c6c7187c
Pedro de Castilho <pedrodecastilho@protonmail.com>
2022-01-24 16:42:11 -0300

    add globalstyle

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,36 @@
+import appConfig from "../config.json";
+
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: "Open Sans", sans-serif;
+      }
+      /* App fit Height */
+      html,
+      body,
+      #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */
+    `}</style>
+  );
+}
+
 function Title(props) {
   const Tag = props.tag;
   return (
@@ -5,7 +38,7 @@ function Title(props) {
       <Tag>{props.children}</Tag>
       <style jsx>{`
         ${Tag} {
-          color: red;
+          color: ${appConfig.theme.colors.neutrals["000"]};
           font-size: 24px;
           font-weight: 600;
         }
@@ -17,6 +50,7 @@ function Title(props) {
 function HomePage() {
   return (
     <div>
+      <GlobalStyle />
       <Title tag="h2">Boas vindas de volta!</Title>
       <h2>Discord - Alura Matrix</h2>
     </div>
