e057f9f7a285c0f60fba30c38a2e581a238daafd
Pedro de Castilho <pedrodecastilho@protonmail.com>
2022-01-24 16:34:52 -0300

    add first components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+function Title(props) {
+  const Tag = props.tag;
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+          font-size: 24px;
+          font-weight: 600;
+        }
+      `}</style>
+    </>
+  );
+}
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      <Title tag="h2">Boas vindas de volta!</Title>
+      <h2>Discord - Alura Matrix</h2>
+    </div>
+  );
 }
 
 export default HomePage;
